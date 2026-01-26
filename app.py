ad5bfd7eff43722184e90f9aed8cf8e6623bfd4f
Vitaliy Ivanov <ivanov.devwork@gmail.com>
2026-01-26 14:33:33 +0200

    Додавання і видалення акаунту. Функції редагування і видалення задачі. Фільтр статусу і пріорітетності задач

--- app.py
+++ app.py
@@ -1,32 +1,142 @@
-#__init__.py
-from flask import Flask, redirect, render_template, request, url_for, session
+# app.py
+
+from flask import Flask, redirect, render_template, request, url_for, session, flash
 from dotenv import load_dotenv
+from datetime import datetime
 import os
+import database as db
+
 
 load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv('secret_key')
+db.init_database()
 
-@app.route("/home")
+#
+@app.route('/')
+def welcome():
+    return render_template('welcome.html')
+
+#
+@app.route("/index")
 def home():
     username = session.get('username')
     tasks = ["Вивчити Flask", "Зробити CRUD", "Створити Telegram-бота"]
-    return render_template('home.html', user=username, todo_list=tasks)
+    return render_template('index.html', username=username, todo_list=tasks)
 
-@app.route("/", methods=['GET', 'POST'])
+#
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     message = None
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if password == '123':
+        user = db.get_user(username)
+        if user and user[2] == password:
             session['username'] = username
-            return redirect(url_for('home'))
+            return redirect(url_for('tasks'))
         else:
             message = 'Неправильний логін або пароль'
 
     return render_template('login.html', message_html=message)
+
+#
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    message = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        age = request.form['age']
+        try:
+            db.add_user(username, password, age)
+            session['username'] = username
+            flash("Реєстрація пройшла успішно")
+            return redirect(url_for('tasks'))
+        except Exception as e:
+            message = "Користувач вже існує"
+    return render_template('register.html', message=message)
+
+#
+@app.route("/delete_account", methods=['POST'])
+def delete_account():
+    if "username" not in  session:
+        return redirect(url_for('login'))
+    user = db.get_user(session['username'])
+    user_id = user[0]
+    db.delete_user(user_id)
+    session.clear()
+    print("Акаунт успішно видалено")
+    flash("Акаунт успішно видалено")
+    return redirect(url_for('welcome'))
+
+#
+priority_map = {1: "Низький", 2: "Середній", 3: "Високий"}
+@app.route("/tasks", methods=['GET', 'POST'])
+def tasks():
+    if "username" not in session:
+        return redirect(url_for('login'))
+
+    user = db.get_user(session['username'])
+    user_id = user[0]
+    if request.method == 'POST':
+        task_id = request.form['task_id']
+        done_value = 'done' in request.form
+        db.toggle_task_done(task_id, done_value)
+
+    status = request.args.get('status')
+    priority = request.args.get('priority')
+
+    todo_list = db.get_filtr_tasks(user_id, status, priority)
+    print(todo_list)
+    return render_template(
+        'tasks.html',
+        todo_list=todo_list,
+        priority_map=priority_map,
+        status=status,
+        priority=priority)
+
+#
+@app.route("/delete_task/<int:task_id>")
+def delete_task(task_id):
+    db.delete_task(task_id)
+    return redirect(url_for('tasks'))
+
+#
+@app.route('/add_task', methods=['GET', 'POST'])
+def add_task():
+    if "username" not in session:
+        return redirect(url_for('login'))
+
+    user = db.get_user(session['username'])
+    user_id = user[0]
+
+    if request.method == 'POST':
+        description = request.form['description']
+        creat_at = datetime.now().date()
+        due_at = request.form['due_at']
+        priority = request.form['priority']
+        db.add_task(user_id, description, creat_at, due_at, priority)
+        return redirect(url_for('tasks'))
+    return render_template('add_task.html')
+
+#
+@app.route('/edit_task/<int:task_id>', methods=['GET', 'POST'])
+def edit_task(task_id):
+    if "username" not in session:
+        return redirect(url_for('login'))
+
+    task = db.get_task(task_id)
+    if request.method == 'POST':
+        description = request.form['description']
+        due_at = request.form['due_at']
+        priority = request.form['priority']
+        db.update_task(task_id, description, due_at, priority)
+        flash("Задача оновлена")
+        return redirect(url_for('tasks'))
+    return render_template('edit_task.html', task=task, priority_map=priority_map)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,124 @@
+# database
+
+import sqlite3
+
+DB_NAME = "database.db"
+
+def init_database():
+    conn = sqlite3.connect(DB_NAME)
+    curs = conn.cursor()
+
+    # Таблиця користувачів
+    curs.execute("""CREATE TABLE IF NOT EXISTS users (
+                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 username TEXT UNIQUE,
+                 password TEXT NOT NULL,
+                 age INTEGER NOT NULL
+    )""")
+
+    # Таблиця завдань
+    curs.execute("""CREATE TABLE IF NOT EXISTS tasks (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER,
+                description TEXT,
+                created_at TIMESTAMP NOT NULL,
+                due_at TIMESTAMP NOT NULL,
+                priority INTEGER NOT NULL,
+                done BOOLEAN NOT NULL,
+                FOREIGN KEY("user_id") REFERENCES users(id)
+    )""")
+    conn.commit()
+    conn.close()
+
+#
+def add_user(username, password, age):
+    conn = sqlite3.connect(DB_NAME)
+    curs = conn.cursor()
+    curs.execute("INSERT INTO users (username, password, age) VALUES (?, ?, ?)", (username, password, age))
+    conn.commit()
+    conn.close()
+#
+def get_user(username):
+    conn = sqlite3.connect(DB_NAME)
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM users WHERE username = ?", (username,))
+    user = curs.fetchone()
+    conn.close()
+    return user
+
+#
+def delete_user(username):
+    conn = sqlite3.connect(DB_NAME)
+    curs = conn.cursor()
+    curs.execute("DELETE FROM tasks WHERE user_id = ?", (username,))
+    curs.execute("DELETE FROM users WHERE id = ?", (username,))
+    conn.commit()
+    conn.close()
+
+#
+def add_task(user_id, description, created_at, due_at, priority):
+    conn = sqlite3.connect(DB_NAME)
+    curs = conn.cursor()
+    curs.execute("""INSERT INTO tasks (user_id, description, created_at, due_at, priority, done)
+                    VALUES (?, ?, ?, ?, ?, 0)""", (user_id, description, created_at, due_at, priority))
+    conn.commit()
+    conn.close()
+
+
+#
+def get_task(task_id):
+    conn = sqlite3.connect(DB_NAME)
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM tasks WHERE id = ?", (task_id,))
+    task = curs.fetchone()
+    conn.close()
+    return task
+#
+def delete_task(task_id):
+    conn = sqlite3.connect(DB_NAME)
+    curs = conn.cursor()
+    curs.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
+    conn.commit()
+    conn.close()
+
+#
+def update_task(task_id, description, due_at, priority):
+    conn = sqlite3.connect(DB_NAME)
+    curs = conn.cursor()
+    curs.execute("""UPDATE tasks SET description = ?,
+                 due_at = ?,
+                 priority = ?
+                 WHERE id = ?""", (description, due_at, priority, task_id))
+    conn.commit()
+    conn.close()
+
+#
+def toggle_task_done(task_id, done_value):
+    conn = sqlite3.connect(DB_NAME)
+    curs = conn.cursor()
+    curs.execute("UPDATE tasks SET done = ? WHERE id = ?", (done_value, task_id))
+    conn.commit()
+    conn.close()
+
+#
+def get_filtr_tasks(user_id, status=None, priority=None):
+    conn = sqlite3.connect(DB_NAME)
+    curs = conn.cursor()
+
+    query = """SELECT * FROM tasks WHERE user_id = ?"""
+    params = [user_id]
+
+    if status == "done":
+        query += " AND done = 1"
+    elif status == "not_done":
+        query += " AND done = 0"
+
+    if priority:
+        query += " AND priority = ?"
+        params.append(priority)
+
+    curs.execute(query, params)
+    tasks = curs.fetchall()
+    conn.close()
+    return tasks
+
